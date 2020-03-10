5de9fc3e16cfd6d262f71f22f259c76114dc75c6
tmanoj526 <t.manoj526@gmail.com>
2020-03-10 16:48:11 +0530

    Updates

---
+++ precache-manifest.84249c2d6227f7ca35c7ead05025fa5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e2b5e27ca1a3b935b88562c4e9b141",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "dde4edf6ae52fbcb0c85",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "345bcae723a08e5770f5",
+    "url": "/contactmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dde4edf6ae52fbcb0c85",
+    "url": "/contactmanager/static/js/2.8063195d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/contactmanager/static/js/2.8063195d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345bcae723a08e5770f5",
+    "url": "/contactmanager/static/js/main.2078144d.chunk.js"
+  },
+  {
+    "revision": "334dc22ba72e24f44108",
+    "url": "/contactmanager/static/js/runtime-main.e0e683c2.js"
+  }
+]);
